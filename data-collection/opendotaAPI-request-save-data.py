12da8ced34d88239e36119df141bfcc1352dcc4f
markosviggiato <markosviggiato@gmail.com>
2020-07-30 12:09:13 -0600

    refactor code, removing legacy code and improving comments

--- data-collection/opendotaAPI-request-save-data.py
+++ data-collection/opendotaAPI-request-save-data.py
@@ -1,76 +1,56 @@
+# =========================
+# Author: Markos Viggiato 
+# Date: May 13, 2020
+# =========================
+
+# import necessary libraries
 import requests
 import json
 import os
 import csv
 import time
 
+# define necessary data structures and directories
 list_match_id = list()
 list_existing_id = list()
 list_to_request_id = list()
 output_directory = "matches/"
 
-# Open csv file with all matches IDs to make requests to OpenDota API
+# open csv file with all matches IDs to make requests to OpenDota API
 with open("match_ids.csv", encoding="utf8") as csvfile:
 	readCSV = csv.reader(csvfile, delimiter=',')
-	next(readCSV) # skip header
+	# skip header
+	next(readCSV) 
 	for row in readCSV:
 		list_match_id.append(row[0])
 
-list_match_id_unique = list(set(list_match_id)) # use 'set' to get unique values from list while already converting back to list
+# use 'set' to get unique values from list while already converting back to list
+list_match_id_unique = list(set(list_match_id)) 
 print("Number of unique matches in the dataset: ", len(list_match_id_unique))
 print("------------------------------------------")
 
+# define iteration and counter variables
 iteration = 0
 number_requests_made = 0
 
-for file in os.listdir(output_directory):
-	file_id = (file.split('-')[1]).split('.')[0]
-	list_existing_id.append(file_id)
-
-print("Number of existing matches : " + str(len(list_existing_id)))
-
-# existing_id_file = open('list_existing_id.txt', 'r')
-# for id in existing_id_file:
-# 	list_existing_id.append(id)
-# print("Number of existing matches : " + str(len(list_existing_id)))
-
-
-# for id in list_match_id_unique:
-# 	if id not in list_existing_id:
-# 		list_to_request_id.append(id)
-
-# with open('list_to_request_id.txt', 'w') as f:
-#     for item in list_to_request_id:
-#         f.write("%s\n" % item)
-
-        
-# with open('list_existing_id.txt', 'w') as f:
-#     for item in list_existing_id:
-#         f.write("%s\n" % item)
-
-
+# loop through the list of match IDs to obtain the match file (in json format)
 for id in list_match_id_unique:
-	print(id)
-	if id in list_existing_id:
-		print("exist...")
-		continue
-
-	print("DO NOT exist...")
 	print("Iteration...", iteration)
 	iteration += 1
+	
 	try:
-		# Request data for a specific match using opendota API
+		# request data for a specific match using the opendota API
 		r = requests.get("https://api.opendota.com/api/matches/" + id)
-		number_requests_made += 1
 		if r.status_code != 200:
 			exit(0)
-
-		# Convert response to json and save into a file
+		number_requests_made += 1
+		
+		# convert response to json and save into a file
 		json_match_info = json.loads(r.text)
 		with open(output_directory + "match-" + id + ".json", 'w') as f:
 			json.dump(json_match_info, f)
 
-		# Sleep for 0.5 sec to be in accordance with OpenDota policies and avoid IP blocking by the OpenDota API
+		# sleep for 0.8 sec to make sure it is in accordance with OpenDota policies and avoid IP blocking by the OpenDota API
 		time.sleep(0.8)
 		
 	except:
@@ -78,3 +58,12 @@ for id in list_match_id_unique:
 		exit(0)
 
 print("Number of successfull requests: ", number_requests_made)
+
+## Uncomment this piece of code to check the number of match files in the output directory (if necessary)
+# for file in os.listdir(output_directory):
+# 	file_id = (file.split('-')[1]).split('.')[0]
+# 	list_existing_id.append(file_id)
+
+# print("Number of existing matches : " + str(len(list_existing_id)))
+# print("------------------------------------------")
+##
